d70da50e0445459ce81836b11e6d2056f7b85641
Andrea Ostuni <andrea.ostuni@polito.it>
2024-06-25 18:52:04 +0200

    refactor algo import from mpc

--- rl_zoo3/utils.py
+++ rl_zoo3/utils.py
@@ -19,8 +19,7 @@ from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.sb2_compat.rmsprop_tf_like import RMSpropTFLike  # noqa: F401
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecEnv, VecFrameStack, VecNormalize
-from mpc_baselines.ppo import MPCPPO
-from mpc_baselines.sac import MPCSAC
+from mpc_baselines import MPCPPO, MPCSAC
 
 # For custom activation fn
 from torch import nn as nn
